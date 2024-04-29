caaa6d4f3f07b3f6a4cc349f09dcce16eda4d796
lieraldev <ssharp0614@gmail.com>
2024-04-29 21:00:41 +0900

    회원가입 페이지 추가

---
+++ src/app/auth/signup/page.tsx
@@ -0,0 +1,41 @@
+import {Button, Card, Checkbox, Label, TextInput} from "flowbite-react";
+import type {Metadata} from "next";
+import Link from "next/link";
+
+export const metadata: Metadata = {
+    title: "회원가입",
+    description: "와인 시커 로그인 페이지 입니다.",
+};
+
+export default function login() {
+    return (
+        <Card className="mt-10 w-4/5 mx-auto sm:w-full sm:mt-0 sm:max-w-sm">
+            <h1 className={"text-2xl font-normal dark:text-white"}>회원가입</h1>
+            <form className="flex flex-col gap-4">
+                <div>
+                    <div className="mb-2 block">
+                        <Label htmlFor="email1" value="이메일"/>
+                    </div>
+                    <TextInput id="email1" type="email" placeholder="example@exmaple.com" required/>
+                </div>
+                <div>
+                    <div className="mb-2 block">
+                        <Label htmlFor="password1" value="비밀번호"/>
+                    </div>
+                    <TextInput id="password1" type="password" required/>
+                </div>
+                <div>
+                    <div className="mb-2 block">
+                        <Label htmlFor="password1comfirm" value="비밀번호 확인"/>
+                    </div>
+                    <TextInput id="password1comfirm" type="password" required/>
+                </div>
+                <div className="flex items-center gap-2">
+                    <Checkbox id="privacy-agree" color={"primary"} required/>
+                    <Label htmlFor="privacy-agree"><Link href={"/privacy"} className={"text-rose-600 dark:text-rose-700"}>개인정보처리방침</Link>에 동의합니다</Label>
+                </div>
+                <Button type="submit" color={"primary"}>회원가입</Button>
+            </form>
+        </Card>
+    )
+}
