3d7ca1e9754ed82075f07967438da9ad4d85b426
lieraldev <ssharp0614@gmail.com>
2024-04-29 21:00:03 +0900

    로그인 페이지 업데이트

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -1,5 +1,6 @@
 import {Button, Card, Checkbox, Label, TextInput} from "flowbite-react";
 import type {Metadata} from "next";
+import Link from "next/link";
 
 export const metadata: Metadata = {
     title: "로그인",
@@ -8,8 +9,8 @@ export const metadata: Metadata = {
 
 export default function login() {
     return (
-        <Card className="w-full sm:mx-auto sm:max-w-sm">
-            <h1 className={"text-2xl font-normal"}>로그인</h1>
+        <Card className="mt-10 w-4/5 mx-auto sm:w-full sm:mt-0 sm:max-w-sm">
+            <h1 className={"text-2xl font-normal dark:text-white"}>로그인</h1>
             <form className="flex flex-col gap-4">
                 <div>
                     <div className="mb-2 block">
@@ -24,10 +25,12 @@ export default function login() {
                     <TextInput id="password1" type="password" required />
                 </div>
                 <div className="flex items-center gap-2">
-                    <Checkbox id="remember" />
+                    <Checkbox id="remember" color={"primary"} />
                     <Label htmlFor="remember">로그인 상태 유지하기</Label>
                 </div>
-                <Button type="submit" className={"bg-rose-600 enabled:hover:bg-rose-700"}>로그인</Button>
+                <Button type="submit" color={"primary"}>로그인</Button>
+                <Link className={"text-center"} href={"/auth/password-reset"}>비밀번호 초기화</Link>
+                <Link className={"text-center"} href={"/auth/signup"}>회원가입</Link>
             </form>
         </Card>
     )
