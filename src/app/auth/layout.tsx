5c04fbe46abc3a147914c6a29d4b730176587d64
lieraldev <ssharp0614@gmail.com>
2024-04-29 21:00:25 +0900

    로그인 페이지 레이아웃 업데이트

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,6 +1,6 @@
 export default function Layout ({children}: {children: React.ReactNode}) {
     return (
-        <div className={"h-screen flex flex-col justify-center "}>
+        <div className={"h-screen flex flex-col justify-space-between md:justify-center"}>
             {children}
         </div>
     )
