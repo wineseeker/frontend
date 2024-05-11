512b68565b94274a97a7ddd4ce26e8931e9b8996
lieraldev <ssharp0614@gmail.com>
2024-05-11 14:07:52 +0900

    브랜드 추가

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,2 +1,13 @@
+import {Navbar, NavbarBrand} from "flowbite-react";
+import Link from "next/link";
+
 export default function MainLayout({children}: {children:React.ReactNode}) {
+    return (
+        <>
+            <Navbar>
+                <NavbarBrand as={Link} href={'/'}><span className={'text-xl'}>와인 시커</span></NavbarBrand>
+            </Navbar>
+            {children}
+        </>
+    );
 }
