8dfee570794b4afef144cdd1ee9c012d2ff83669
lieraldev <ssharp0614@gmail.com>
2024-05-11 14:02:29 +0900

    레이아웃 파일 이동 반영

--- src/app/auth/layout.tsx
+++
@@ -1,7 +0,0 @@
-export default function Layout ({children}: {children: React.ReactNode}) {
-    return (
-        <div className={"h-screen flex flex-col justify-space-between md:justify-center"}>
-            {children}
-        </div>
-    )
-}
