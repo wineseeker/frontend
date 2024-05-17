826d940659bbc5b1aa3d4bcb59e4ae9bb2d1ee70
lieraldev <ssharp0614@gmail.com>
2024-05-17 18:58:28 +0900

    로그인 기능 예외처리 추가

--- src/app/components/auth/login-form.tsx
+++ src/app/components/auth/login-form.tsx
@@ -1,36 +1,45 @@
 'use client'
 
-import {Button, Checkbox, Label, TextInput} from "flowbite-react";
+import {Alert, Button, Checkbox, Label, TextInput} from "flowbite-react";
 import {login} from "@/app/lib/login";
 import {useEffect, useRef} from "react";
+import { useFormState } from "react-dom";
+
+const initialState = {
+    message: '',
+}
 
 export function LoginForm() {
     const emailInputRef = useRef<HTMLInputElement>(null);
+    const [state, formAction] = useFormState(login, initialState)
 
     useEffect(() => {
         emailInputRef.current?.focus();
     },[])
 
     return (
-        <form className="flex flex-col gap-4" action={login}>
-            <div>
-                <div className="mb-2 block">
-                    <Label htmlFor="email" value="이메일" />
+        <>
+            {state?.message && <Alert color="failure">{state?.message}</Alert>}
+            <form className="flex flex-col gap-4" action={formAction}>
+                <div>
+                    <div className="mb-2 block">
+                        <Label htmlFor="email" value="이메일" />
+                    </div>
+                    <TextInput id="email" name="email" type="email" placeholder="example@exmaple.com" color={"rose"} ref={emailInputRef} required />
                 </div>
-                <TextInput id="email" name="email" type="email" placeholder="example@exmaple.com" color={"rose"} ref={emailInputRef} required />
-            </div>
-            <div>
-                <div className="mb-2 block">
-                    <Label htmlFor="password" value="비밀번호" />
+                <div>
+                    <div className="mb-2 block">
+                        <Label htmlFor="password" value="비밀번호" />
+                    </div>
+                    <TextInput id="password" name={"password"} type="password" color={"rose"} required />
                 </div>
-                <TextInput id="password" name={"password"} type="password" color={"rose"} required />
-            </div>
-            <div className="flex items-center gap-2">
-                <Checkbox id="remember" name="remember" color={"rose"} />
-                <Label htmlFor="remember">로그인 상태 유지하기</Label>
-            </div>
-            <Button type="submit" color={"rose"}>로그인</Button>
-        </form>
+                <div className="flex items-center gap-2">
+                    <Checkbox id="remember" name="remember" color={"rose"} />
+                    <Label htmlFor="remember">로그인 상태 유지하기</Label>
+                </div>
+                <Button type="submit" color={"rose"}>로그인</Button>
+            </form>
+        </>
     )
 }
 
--- src/app/lib/login.ts
+++ src/app/lib/login.ts
@@ -3,45 +3,57 @@
 import {redirect} from "next/navigation";
 import {cookies} from "next/headers";
 
-export async function login(formData: FormData) {
-    console.log(formData);
-
-    const res = await fetch('http://localhost:8000/login', {
-        method: 'POST',
-        cache: 'no-store',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            email: formData.get('email')?.toString(),
-            password: formData.get('password')?.toString()
+export async function login(initialState: any, formData: FormData) {
+    try {
+        const res = await fetch('http://localhost:8000/login', {
+            method: 'POST',
+            cache: 'no-store',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                email: formData.get('email')?.toString(),
+                password: formData.get('password')?.toString()
+            })
         })
-    })
 
-    if (res.status === 200) {
-        const body = await res.json();
-        console.log(body);
+        if (res.status === 200) {
+            const body = await res.json();
+            console.log(body);
 
-        type cookieOption = {
-            httpOnly?: boolean
-            path?: string
-            expires?: number
-            secure?: boolean
+            type cookieOption = {
+                httpOnly?: boolean
+                path?: string
+                expires?: number
+                secure?: boolean
+            }
+
+            const cookieOption: cookieOption = {
+                httpOnly: true,
+                path: '/',
+            }
+
+            if (formData.get('remember')?.toString() === 'on') {
+                const expires = body.expiresAt.toString()
+                cookieOption.expires = Date.parse(expires)
+            }
+            cookies().set('session', body.id.toString(), cookieOption)
+        } else if (res.status === 400) {
+            return {
+                message: '잘못된 이메일 혹은 비밀번호를 입력하셨습니다.',
+            }
+        } else {
+            return {
+                message: '백엔드에 문제가 발생했습니다. 나중에 다시 시도해보세요.',
+            }
         }
-
-        const cookieOption: cookieOption = {
-            httpOnly: true,
-            path: '/',
+    } catch (e) {
+        console.error(e)
+        return {
+            message: '서버에 문제가 발생했습니다. 나중에 다시 시도해보세요.',
         }
-
-        if (formData.get('remember')?.toString() === 'on') {
-            const expires = body.expiresAt.toString()
-            cookieOption.expires = Date.parse(expires)
-        }
-
-        cookies().set('session', body.id.toString(), cookieOption)
-        console.log(cookieOption)
-        return redirect(`/`)
     }
+    // try-catch 문 안에 redirect 문을 넣으면 작동하지 않는 문제가 있어서 밖에 넣습니다.
+    return redirect(`/`)
 }
