595550ba30d594aa5d00e21b03bbbcebafe4b20a
liberaldev <j0321@mju.ac.kr>
2024-06-04 11:39:20 +0900

    개인정보처리방침 클래스 순서 변경

--- src/app/(main)/privacy/page.tsx
+++ src/app/(main)/privacy/page.tsx
@@ -2,7 +2,7 @@ import PrivacyPolicy from '@/markdown/privacy-policy.mdx'
 
 export default function Page() {
     return (
-        <article className={"mx-4 md:container md:mx-auto prose"}>
+        <article className={"mx-4 prose md:container md:mx-auto"}>
             <PrivacyPolicy />
         </article>
 
