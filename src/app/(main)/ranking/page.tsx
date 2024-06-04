0d7ef5c7634ccdb2f6ea7e14ba582eab3fded971
liberaldev <j0321@mju.ac.kr>
2024-06-04 16:04:27 +0900

    랭킹 페이지 준비중 추가

---
+++ src/app/(main)/ranking/page.tsx
@@ -0,0 +1,20 @@
+import type {Metadata} from "next";
+import {SurveyForm} from "@/app/components/survey/survey-form";
+
+export const metadata: Metadata = {
+    title: "랭킹",
+    description: "비비노 평점 랭킹을 기반으로한 랭킹입니다",
+    //준비중일때는 색인 안되게 하기
+    robots: {
+        index: false,
+    }
+};
+
+
+export default function Page() {
+    return (
+        <div className={"min-h-96 md:container md:mx-auto"}>
+            <p className={"text-lg"}>준비 중 입니다.</p>
+        </div>
+    )
+}
