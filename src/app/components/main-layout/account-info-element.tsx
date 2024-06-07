7f45dde830296fe0ff63de9df6b8d2a31d0a3c97
liberaldev <j0321@mju.ac.kr>
2024-06-08 01:42:45 +0900

    account-info-element.tsx edit

--- src/app/components/main-layout/account-info-element.tsx
+++ src/app/components/main-layout/account-info-element.tsx
@@ -7,20 +7,20 @@ import Link from "next/link";
 
 export function AccountInfoElement({accountInfo}: {accountInfo: AccountInfo}) {
     return (
-        <List unstyled className="divide-y space-y-0 divide-gray-200 dark:divide-gray-700 text-black bold-list-numbers items-center">
-            <Link href={"/account/email-change"}>
-                <List.Item className={"flex px-3 py-2 sm:py-3 hover:bg-slate-50"}>
+        <List unstyled className="divide-y space-y-0 divide-gray-200 dark:divide-gray-700 text-black items-center">
+            <List.Item>
+                <Link href={"/account/email-change"} className={"flex px-3 py-2 sm:py-3 hover:bg-slate-50"}>
                     <div className={"flex flex-col"}>
                         <div>이메일</div>
                         <div className={"text-sm text-gray-500"}>{accountInfo.email}</div>
                     </div>
                     <FaAngleRight className={"ml-auto my-auto"} />
-                </List.Item>
-            </Link>
+                </Link>
+            </List.Item>
             <List.Item className={"flex px-3 py-2 hover:bg-slate-50 sm:py-3"}>
                 <div className={"flex flex-col"}>
                     <div>비밀번호 변경하기</div>
-                    <div className={"text-sm text-gray-500"}>비밀번호를 변경합니다</div>
+                    <div className={"text-sm text-gray-500"}>비밀번호를 변경할 수 있습니다</div>
                 </div>
                 <FaAngleRight className={"ml-auto my-auto"} />
             </List.Item>
