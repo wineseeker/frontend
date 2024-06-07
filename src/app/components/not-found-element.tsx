a4be293390617346b5e5b67cec46e11498947b46
liberaldev <j0321@mju.ac.kr>
2024-06-07 22:29:40 +0900

    not found 관련 코드 리팩토링

---
+++ src/app/components/not-found-element.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+import {FaCircleInfo} from "react-icons/fa6";
+import {Button} from "flowbite-react";
+import Link from "next/link";
+
+export function NotFoundElement() {
+    return (
+        <div className="flex flex-col justify-space-between text-center gap-4 md:container md:mx-auto">
+            <FaCircleInfo className={"mx-auto text-8xl text-rose-600"}/>
+            <h1 className={"text-4xl font-bold"}>페이지를 찾을 수 없습니다</h1>
+            <p className={"text-lg"}>잘못된 주소를 입력하셨거나, 페이지 주소가 변경되거나 해당 페이지가 삭제되었습니다.</p>
+            <Button as={Link} href={"/"} color={"rose"} className={"mx-auto"}>
+                <span className={"text-lg"}>홈으로 가기</span>
+            </Button>
+        </div>
+    )
+}
---
+++ src/app/lib/not-found-metadata.ts
@@ -0,0 +1,6 @@
+import {Metadata} from "next";
+
+export const notFoundMetadata: Metadata = {
+    title: '페이지를 찾을 수 없습니다',
+    description: undefined
+}
