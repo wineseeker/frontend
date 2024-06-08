8daa3b6fbeef5dde8cea815756840acbe268a89a
liberaldev <j0321@mju.ac.kr>
2024-06-08 15:30:11 +0900

    검색에 제목 추가

--- src/app/(main)/search/page.tsx
+++ src/app/(main)/search/page.tsx
@@ -1,25 +1,39 @@
 import {Header} from "@/app/components/main-layout/header";
 import {SearchResultList} from "@/app/components/main-layout/search/search-result-list";
 import {wineSearch} from "@/app/lib/wine-search";
+import type {Metadata} from "next";
+
+export async function generateMetadata({searchParams}: {searchParams: { // noinspection JSUnusedLocalSymbols
+        [key: string]: string | undefined }}): Promise<Metadata> {
+    if (!searchParams.q) {
+        return ({
+            title: '검색'
+        })
+    }
+
+    return ({
+        title: `"${searchParams.q}"에 대한 검색 결과`
+    })
+}
 
 export default async function Search({searchParams}: {searchParams: { // noinspection JSUnusedLocalSymbols
         [key: string]: string | undefined }}) {
     const q = searchParams.q
 
-    if (typeof q !== undefined) {
-        const initialSearchResult = await wineSearch(q as string);
-
+    if (!q) {
         return (
             <div className={"mt-4 mx-3 flex flex-col gap-4 md:container md:mx-auto"}>
-                <Header>{searchParams.q}의 검색 결과</Header>
-                {(initialSearchResult.length > 0) && <SearchResultList initialSearchResult={initialSearchResult} />}
+                <Header>상단의 검색창에 검색어를 입력하세요</Header>
             </div>
         )
     }
 
+    const initialSearchResult = await wineSearch(q as string);
+
     return (
         <div className={"mt-4 mx-3 flex flex-col gap-4 md:container md:mx-auto"}>
-            <Header>상단의 검색창에 검색어를 입력하세요</Header>
+            <Header>{searchParams.q}의 검색 결과</Header>
+            {(initialSearchResult.length > 0) && <SearchResultList initialSearchResult={initialSearchResult} />}
         </div>
     )
 }
