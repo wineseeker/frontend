307fbd6236198ac9f5970fccc0054f30d2baa3e9
liberaldev <j0321@mju.ac.kr>
2024-06-08 17:04:25 +0900

    검색 결과 코드 개선

--- src/app/components/main-layout/search/search-result-list.tsx
+++ src/app/components/main-layout/search/search-result-list.tsx
@@ -7,6 +7,7 @@ import {useSearchParams} from "next/navigation";
 import {wineSearch} from "@/app/lib/wine-search";
 import {List, Spinner} from "flowbite-react";
 import Link from "next/link";
+import {regionName} from "@/app/lib/region-name";
 
 export function SearchResultList({initialSearchResult}: {initialSearchResult: Wine[]}) {
     const [searchResult, setSearchResult] = useState(initialSearchResult);
@@ -49,10 +50,18 @@ export function SearchResultList({initialSearchResult}: {initialSearchResult: Wi
 
     const searchResultItem = searchResult.map((item) => (
         <List.Item key={item.id}>
-            <Link href={"/wine/" + item.id} className={"block py-4"}>
-                <div className={"inline-flex flex-row"}>
+            <Link href={"/wine/" + item.id} className={"block py-3"}>
+                <div className={"inline-flex flex-col"}>
                     <div className={"hover:underline"}>
                         {item.name}
+                    </div>
+                    <div className={"item-summary-information"}>
+                        <div>국가:&nbsp;{(item.countryCode !== undefined)&& regionName(item.countryCode)}</div>
+                        <div>산도:&nbsp;{item.acidity.toFixed(3)}</div>
+                        <div>당도:&nbsp;{item.sweetness.toFixed(3)}</div>
+                        <div>타닌:&nbsp;{item.tannin.toFixed(3)}</div>
+                        <div>바디감:&nbsp;{item.tannin.toFixed(3)}</div>
+                        <div>비비노 평균 평점:&nbsp;{item.ratingAverage}(리뷰 개수: {item.ratingCount}개)</div>
                     </div>
                 </div>
             </Link>
--- src/app/lib/region-name.ts
+++ src/app/lib/region-name.ts
@@ -1,4 +1,21 @@
+import countries from "i18n-iso-countries"
+
 export function regionName(countryCode: string) {
     const regionNames = new Intl.DisplayNames(['ko'], {type: 'region'});
-    return regionNames.of(countryCode.toUpperCase())
+
+    let alpha2CountryCode = countryCode.toUpperCase();
+
+    if (alpha2CountryCode === "UNK") {
+        alpha2CountryCode = "XK"
+    }
+
+    if (alpha2CountryCode.length > 3) {
+        countries.alpha3ToAlpha2(alpha2CountryCode)
+    }
+
+    try {
+        return regionNames.of(alpha2CountryCode)
+    } catch {
+        return alpha2CountryCode
+    }
 }
