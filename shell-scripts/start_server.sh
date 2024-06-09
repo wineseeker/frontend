8db1b03d6c48222c9b9846f8ebf460dc73a02ccd
liberaldev <j0321@mju.ac.kr>
2024-06-10 00:27:19 +0900

    start_server.sh 조정

--- shell-scripts/start_server.sh
+++ shell-scripts/start_server.sh
@@ -1,1 +1,15 @@
-pm2 start "pnpm run start" --name wine-seeker-frontend
+#!/bin/bash
+
+# 사용할 프로세스 이름
+PROCESS_NAME="wine-seeker-frontend"
+
+# 프로세스가 실행 중인지 확인
+if pm2 list | grep -q "$PROCESS_NAME"; then
+    # 프로세스가 실행 중일 경우
+    echo "$PROCESS_NAME is running. Restarting..."
+    NODE_ENV=production pm2 restart "$PROCESS_NAME" --update-env
+else
+    # 프로세스가 실행 중이지 않을 경우
+    echo "$PROCESS_NAME is not running. Starting..."
+    NODE_ENV=production pm2 start "pnpm run start" --name "$PROCESS_NAME"
+fi
