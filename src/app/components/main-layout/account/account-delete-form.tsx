5b8c7b99a2d7b1fb40696ec94d62220dc86e21fb
liberaldev <j0321@mju.ac.kr>
2024-06-10 16:23:36 +0900

    회원 탈퇴 개선

--- src/app/components/main-layout/account/account-delete-form.tsx
+++ src/app/components/main-layout/account/account-delete-form.tsx
@@ -1,16 +1,20 @@
 'use client'
 
-import {Button, Checkbox, Label, TextInput} from "flowbite-react";
+import {Button, Checkbox, Label, TextInput, Toast} from "flowbite-react";
 import {useFormState, useFormStatus} from "react-dom";
 import {deleteAccount} from "@/app/lib/delete-account";
-import {useEffect, useRef, useState} from "react";
+import {useContext, useEffect, useRef, useState} from "react";
 import {useRouter} from "next/navigation";
+import {UuidV4} from "@/app/lib/uuidv4";
+import {FaCheck} from "react-icons/fa6";
+import {ToastContext} from "@/app/components/providers/toast-provider";
 
 const initialState = {
     message: '',
 }
 
 export function AccountDeleteForm() {
+    const { setToasts } = useContext(ToastContext);
     const passwordInputRef = useRef<HTMLInputElement>(null);
     const [state, formAction] = useFormState(deleteAccount, initialState)
     const { pending } = useFormStatus()
@@ -22,7 +26,23 @@ export function AccountDeleteForm() {
     useEffect(() => {
         if (step === 1 && state === undefined) {
             setStep(step + 1)
-            router.refresh()
+            setToasts([{
+                id: UuidV4.generate(),
+                content: (
+                    <>
+                        <div
+                            className="inline-flex h-8 w-8 shrink-0 items-center justify-center rounded-lg bg-green-100 text-green-500 dark:bg-green-800 dark:text-green-200">
+                            <FaCheck className="h-5 w-5"/>
+                        </div>
+                        <div className="ml-3 text-sm font-normal">
+                            회원 탈퇴가 완료되었습니다. 와인 시커를 이용해주셔서 감사합니다. 브라우저 캐시를 지울 때까지 일부 페이지에서에서 아직 로그인이 되어 있는 것처럼 보일 수 있습니다.
+                        </div>
+                        <Toast.Toggle/>
+                    </>
+                ),
+                timeout: 5000
+            }])
+            router.push('/')
         }
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [state]);
@@ -52,9 +72,7 @@ export function AccountDeleteForm() {
                 <Button type="submit" color={"rose"} disabled={pending} className={"ml-auto"}>탈퇴하기</Button>
             </form>
         )
-    } else if (step === 2) {
-        return (
-            <p>회원 탈퇴가 완료되었습니다. 와인 시커를 이용해주셔서 감사합니다.</p>
-        )
+    } else {
+        return null
     }
 }
