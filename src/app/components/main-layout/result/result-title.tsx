5c69c4790e87fc457ca9c9fe9fd227610348beca
liberaldev <j0321@mju.ac.kr>
2024-06-10 19:57:27 +0900

    eslint 방지

--- src/app/components/main-layout/result/result-title.tsx
+++ src/app/components/main-layout/result/result-title.tsx
@@ -14,6 +14,7 @@ export function ResultTitle({dateTime}: {dateTime: string}) {
             // 클라이언트 렌더링 중에는 서울 시간대로 렌더링 했다가 클라이언트 시간대로 변경
             timeZone: isClient? undefined : "Asia/Seoul",
         }).format(new Date(dateTime)) + "에 완료된 설문에 대한 추천 - 와인 시커"
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
     return (
