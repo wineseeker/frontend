c1472e7001af019e045a8ce509b18720c18b3bfe
liberaldev <j0321@mju.ac.kr>
2024-06-11 01:10:40 +0900

    이메일 인증 페이지가 CDN에 캐시 안되도록 수정

--- src/app/(auth)/email-verification/page.tsx
+++ src/app/(auth)/email-verification/page.tsx
@@ -1,17 +1,14 @@
 import type {Metadata} from "next";
-import Link from "next/link";
-import {LoginForm} from "@/app/components/auth/login-form";
-import {logoutAction} from "@/app/lib/logout";
-import { redirect } from 'next/navigation'
 import {EmailVerificationForm} from "@/app/components/auth/email-verification-form";
+import { unstable_noStore as noStore } from 'next/cache';
 
 export const metadata: Metadata = {
     title: "이메일 인증",
     description: "와인 시커 로그인 페이지 입니다.",
 };
 
-export default async function login() {
-
+export default async function Page() {
+    noStore()
     return (
         <>
             <h1 className={"text-2xl font-medium dark:text-white"}>이메일 인증</h1>
