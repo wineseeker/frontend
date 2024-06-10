49d4dfb6652759065ac154d7a0bc03020655cc8e
liberaldev <j0321@mju.ac.kr>
2024-06-10 19:17:07 +0900

    회원 탈퇴 개선

--- src/app/(main)/account/delete/page.tsx
+++ src/app/(main)/account/delete/page.tsx
@@ -1,15 +1,7 @@
 import {Header} from "@/app/components/main-layout/header";
 import {AccountDeleteForm} from "@/app/components/main-layout/account/account-delete-form";
-import {getUserInfo} from "@/app/lib/get-userinfo";
-import {notFound} from "next/navigation";
 
 export default async function Page() {
-    const userInfo = await getUserInfo();
-
-    if (userInfo === null || userInfo === -1) {
-        notFound()
-    }
-
     return (
         <>
             <Header>회원 탈퇴</Header>
--- src/app/components/main-layout/account/account-delete-form.tsx
+++ src/app/components/main-layout/account/account-delete-form.tsx
@@ -47,32 +47,28 @@ export function AccountDeleteForm() {
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [state]);
 
-    if (step === 1) {
-        return (
-            <form className="flex flex-col gap-4" action={formAction}>
-                <div>
-                    <div className="mb-2 block">
-                        <Label htmlFor="password" value="비밀번호"/>
-                    </div>
-                    <TextInput
-                        id="password"
-                        name="password"
-                        type="password"
-                        ref={passwordInputRef}
-                        color={(state?.message) ? "failure" : "rose"}
-                        helperText={state?.message}
-                        required/>
+    return (
+        <form className="flex flex-col gap-4" action={formAction}>
+            <div>
+                <div className="mb-2 block">
+                    <Label htmlFor="password" value="비밀번호"/>
                 </div>
-                <div className="flex items-center gap-2">
-                    <Checkbox id="privacy-agree" color={"rose"} required/>
-                    <Label htmlFor="privacy-agree">
-                        회원 탈퇴를 할 경우 복구가 불가능하며 이에 동의합니다
-                    </Label>
-                </div>
-                <Button type="submit" color={"rose"} disabled={pending} className={"ml-auto"}>탈퇴하기</Button>
-            </form>
-        )
-    } else {
-        return null
-    }
+                <TextInput
+                    id="password"
+                    name="password"
+                    type="password"
+                    ref={passwordInputRef}
+                    color={(state?.message) ? "failure" : "rose"}
+                    helperText={state?.message}
+                    required/>
+            </div>
+            <div className="flex items-center gap-2">
+                <Checkbox id="privacy-agree" color={"rose"} required/>
+                <Label htmlFor="privacy-agree">
+                    회원 탈퇴를 할 경우 복구가 불가능하며 이에 동의합니다
+                </Label>
+            </div>
+            <Button type="submit" color={"rose"} disabled={pending} className={"ml-auto"}>탈퇴하기</Button>
+        </form>
+    )
 }
